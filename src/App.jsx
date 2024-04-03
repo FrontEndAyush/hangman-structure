fcf146be86e8b6eb43929814b76c1f772e5148dc
ayush <iamrealglider@gmail.com>
2024-04-03 23:38:07 +0530

    hangman structure for pc

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <>
+      <div className="flex justify-center mt-[250px]">
+        <h1 className="h-[90px] w-[10px] absolute right-[630px] bg-black"></h1>
+        <h1 className="h-[90px] w-[90px] border-[10px] rounded-full  absolute right-[590px] top-[330px] border-black"></h1>
+        <h1 className="h-[170px] w-[10px] absolute right-[630px] top-[420px] bg-black"></h1>
+        <h1 className="h-[110px] w-[10px] absolute right-[667px] top-[400px] transform rotate-45 bg-black"></h1>
+        <h1 className="h-[110px] w-[10px] absolute right-[600px] top-[400px] transform -rotate-45 bg-black"></h1>
+        <h1 className="h-[110px] w-[10px] absolute right-[670px] top-[530px] transform rotate-45 bg-black"></h1>
+        <h1 className="h-[110px] w-[10px] absolute right-[595px] top-[530px] transform -rotate-45 bg-black"></h1>
+        <h1 className="h-[400px]  bg-black w-[10px]"></h1>
+        <h1 className="w-[200px] h-[10px] text-black bg-black"></h1>
+        <h1 className="w-[200px] absolute bottom-[90px] right-[735px] h-[10px] text-black bg-black"></h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
